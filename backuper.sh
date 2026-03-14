b75c97717ef6493593cfc3abe95e427025b35829
foxtrot222 <foxtrot25012@proton.me>
2026-03-14 19:28:42 +0530

    Initial Commit

---
+++ backuper.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2026 Tirth Kavathiya <tirthkavathiya@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+sudo -v
+
+s_dirs=7
+mount_dir=/run/media/foxtrot/Ventoy/Data
+b_dirs=( "$HOME/Documents" "$HOME/Pictures" "$HOME/Files/College" "$HOME/Files/3D" "$HOME/Files/Org-Nest" "$HOME/Books" "$HOME/Files/Godot/ProjectFiles" )
+
+run_syncthing() {
+    echo "Starting Syncthing in background..."
+    apikey=$(xmllint --xpath 'string(//apikey)' "$HOME/.local/state/syncthing/config.xml")
+
+    s_connection() {
+        curl -s -H "X-API-Key: $apikey" http://127.0.0.1:8384/rest/system/connections | jq '.connections.[].connected'
+    }
+    s_completion() {
+	curl -s -H "X-API-Key: $apikey" http://127.0.0.1:8384/rest/events | jq 'map(select(.type == "FolderCompletion")) | sort_by(.time) | reverse | unique_by(.data.folder) | map(select(.data.completion == 100)) | length'
+    }
+    s_shutdown() {
+        curl -X POST -H "X-API-Key: $apikey" http://127.0.0.1:8384/rest/system/shutdown
+    }
+
+    syncthing --no-browser > /dev/null 2>&1 &
+
+    sleep 5
+    echo "Waiting for Syncthing to establish connection..."
+    while [ "$(s_connection)" != "true" ]; do
+        sleep 5
+    done
+
+    echo "Syncthing connected successfully."
+    sleep 10
+
+    echo "Waiting for Syncthing sync to complete..."
+    while [ "$(s_completion)" != "$s_dirs" ]; do
+        sleep 5
+    done
+
+    echo "Sync completed."
+    sleep 5
+
+    echo "Shutting down Syncthing..."
+    s_shutdown
+    echo "Syncthing shut down."
+}
+
+usb() {
+    echo "Waiting for USB device to be mounted at $mount_dir..."
+    until [ -d $mount_dir ]; do
+        sleep 5
+    done
+
+    echo "USB device connected."
+
+    echo "Creating Backups Archive from Directories..."
+    tar -cJf /tmp/Backups.tar.xz "${b_dirs[@]}"
+    echo "Archive created."
+
+    echo "Moving Archive to USB device..."
+    mv /tmp/Backups.tar.xz "$mount_dir/"
+    echo "Backup moved to USB device."
+
+    echo "Unmounting USB device /dev/sdc1..."
+    sudo umount /dev/sdc1
+    echo "USB device unmounted."
+}
+
+echo "Starting Syncthing sync and USB backup process..."
+
+run_syncthing
+usb
+
+echo "Backup process completed successfully."
---
+++ cleaner.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2026 Tirth Kavathiya <tirthkavathiya@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+sudo -v
+
+flatpak_config_remove() {
+    echo "Removing useless Flatpak config directories..."
+    list=( $(flatpak list --app --columns=application) )
+    var_list=( $(ls ~/.var/app/) )
+
+    for package in "${var_list[@]}"; do
+        present=0
+        for i_package in "${list[@]}"; do
+            if [ "$package" = "$i_package" ]; then
+                present=1
+                break
+            fi
+        done
+        if [ "$present" -ne 1 ]; then
+            echo "Removing unused Flatpak config: $package"
+            rm -rf "$HOME/.var/app/$package"
+        fi
+    done
+    echo "Flatpak config cleanup completed."
+}
+
+echo "Clearing Bash history..."
+history -c && rm ~/.bash_history
+echo "Bash history cleared."
+
+echo "Removing Recent File History..."
+rm ~/.local/share/recently-used.xbel
+echo "Recent File History removed."
+
+echo "Emptying Trash..."
+gio trash --empty
+echo "Trash emptied."
+
+echo "Removing temporary files..."
+sudo rm -rf /tmp/*
+rm -rf ~/.cache/*
+echo "Temporary files removed."
+
+echo "Cleaning no-use packages with DNF..."
+sudo dnf clean packages
+sudo dnf clean all
+echo "Performing autoremove of unnecessary packages..."
+sudo dnf autoremove -y
+echo "Cleaning DNF metadata..."
+sudo dnf clean metadata
+echo "Uninstalling unused Flatpak runtimes and apps..."
+flatpak uninstall --unused -y
+
+flatpak_config_remove
+
+echo "System cleanup completed."
